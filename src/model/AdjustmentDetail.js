7e92b9d1ca61f6230c2b6f677098bbe98f72d2ad
Wendell Wayne Estrada <wendell.estrada@irely.com>
2016-10-28 21:19:34 +0800

    update to build version

---
+++ src/model/AdjustmentDetail.js
@@ -0,0 +1,30 @@
+
+/**
+ * Created by LZabala on 3/27/2015.
+ */
+Ext.define('Inventory.model.AdjustmentDetail', {
+    extend: 'iRely.BaseEntity',
+
+    requires: [
+        'Ext.data.Field'
+    ],
+
+    idProperty: 'intInventoryAdjustmentId',
+
+    fields: [
+        { name: 'intInventoryAdjustmentId', type: 'int' },
+        { name: 'intLocationId', type: 'int', allowNull: true },
+        { name: 'dtmAdjustmentDate', type: 'date', dateFormat: 'c', dateWriteFormat: 'Y-m-d' },
+        { name: 'intAdjustmentType', type: 'int', allowNull: true },
+        { name: 'strAdjustmentNo', type: 'string' },
+        { name: 'strDescription', type: 'string' },
+        { name: 'ysnPosted', type: 'boolean'},
+        { name: 'intSort', type: 'int', allowNull: true }
+    ],
+
+    validators: [
+        { type: 'presence', field: 'intLocationId' },
+        { type: 'presence', field: 'dtmAdjustmentDate' },
+        { type: 'presence', field: 'intAdjustmentType' }
+    ]
+});
---
+++ src/test/mock/Entity.model.AdjustmentNote.js
@@ -0,0 +1,1 @@
+Ext.define('Entity.model.AdjustmentNote', {});
